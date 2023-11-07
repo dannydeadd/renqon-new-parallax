6b8e6dc24b5efa276c284a38bcea86b37521ef44
dannydeadd <98296978+dannydeadd@users.noreply.github.com>
2023-11-06 21:09:25 -0600

    first page done v4

--- src/app/components/main.tsx
+++ src/app/components/main.tsx
@@ -5,8 +5,8 @@ import dynamic from 'next/dynamic';
 
 const Main = () => (
   <section className="bg-black dark:bg-black-50 flex-col place-self-center content-center">
-    <img class="img2" src="/ddd2.gif" alt="" />
-    <img class="img1" src="/ddd.jpg" alt="" />
+    <img id="img2" src="/ddd2.gif" alt="" />
+    <img id="img1" src="/ddd.jpg" alt="" />
     <div id="renqon" className="flex justify-center items-center w-auto h-screen text-8xl z-10">
         <h1 className="">renqon</h1>
     </div>
