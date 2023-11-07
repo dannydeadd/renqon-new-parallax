409dcc0105d14cdec7bf2b84194c7615feb361cb
dannydeadd <98296978+dannydeadd@users.noreply.github.com>
2023-11-06 21:07:59 -0600

    first page done v3

--- src/app/components/main.tsx
+++ src/app/components/main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import dynamic from 'next/dynamic';
 // const DynamicSketch = dynamic(() => import('./Sketch'), { ssr: false });
 
