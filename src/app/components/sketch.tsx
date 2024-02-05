140840a4a03ccb45b11ff07154c328c5c0ed4d19
dannydeadd <98296978+dannydeadd@users.noreply.github.com>
2024-02-04 22:29:01 -0600

    foruth

--- src/app/components/sketch.tsx
+++ src/app/components/sketch.tsx
@@ -46,7 +46,7 @@ const Sketch = () => {
                             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-black">
                                 Payments tool for software companies</h1>
                             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-                                Lorem ipsum dolor sit, amet consectetur adipisicing quas around the world use "renqon" to simplify their financial database.</p>
+                                Lorem ipsum dolor sit, amet consectetur adipisicing quas around the world use renqon to simplify their financial database.</p>
                                 <div id="input-group">
                                     <input type="email" id="input-hero" name="Email" placeholder="renqon news and more" autocomplete="off"/>
                                     <input id="button--submit" value="Subscribe" type="submit"/>
